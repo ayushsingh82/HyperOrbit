56409f1ea9500dc67d31c1675901bff16f706d96
ayushsingh82 <ayushsinghmi711@gmail.com>
2025-08-24 23:42:54 +0530

    fix

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,7 +5,7 @@ import { PrivyProvider } from '@privy-io/react-auth';
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
-      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || "add_me"}
+      appId="cm6ifh5ql004ze7raa9cr0n3k"
       config={{
         // Create embedded wallets for users who don't have a wallet
         embeddedWallets: {
